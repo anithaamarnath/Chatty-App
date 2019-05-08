f90ebe87c98a597cfaa14b768ca3237fb7e0510e
anithaamarnath <anithaamarnath04@gmail.com>
2019-05-08 02:30:33 +0000

    chatBar functionality is added

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -16,7 +16,7 @@ export default function ChatBar(props) {
     return (
 
       <footer className="chatbar">
-      <input   className="chatbar-username" placeholder="Your Name (Optional)"  defaultValue= {props.currentUser}/>
+      <input  className="chatbar-username" placeholder="Your Name (Optional)"  defaultValue= {props.currentUser}/>
       <input onKeyPress = {onKeyPress} className="chatbar-message" name ="messageInput" placeholder="Type a message and hit ENTER"  />
       </footer>
 
